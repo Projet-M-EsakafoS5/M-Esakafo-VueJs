e5946467ecf8d09536bd58d8cd0787cddb9e95f7
Mioty Ifaliana <razafindrabeifaliana@gmail.com>
2025-02-07 20:51:27 +0300

    Update

--- m-esakafo-vue/vite.config.js
+++ m-esakafo-vue/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'https://cors-anywhere.herokuapp.com/https://m-esakafo-1.onrender.com',
+        target: process.env.VITE_API_URL || 'https://m-esakafo-1.onrender.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path
