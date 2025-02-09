2df2c593428c72666206eb703502dffe0d2d4c19
Mioty Ifaliana <razafindrabeifaliana@gmail.com>
2025-02-09 11:19:26 +0300

    Update App.vue

--- m-esakafo-vue/src/App.vue
+++ m-esakafo-vue/src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <div id="app">
-    <!-- <div class="sidebar">
-      <ul>
-        <li><router-link to="/ingredient">Ingredients</router-link></li>
-        <li><router-link to="/mouvement">Mouvement</router-link></li>
-        <li><router-link to="/commandes">Commandes</router-link></li>
-      </ul>
-    </div> -->
     <div class="content">
       <router-view></router-view>
     </div>
@@ -20,16 +13,6 @@ export default {
 </script>
 
 <style>
-/* #app {
-  display: flex;
-} */
-
-.sidebar {
-  width: 400px;
-  background-color: #3d0228;
-  height: 100vh;
-  box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
-}
 
 .sidebar ul {
   list-style-type: none;
